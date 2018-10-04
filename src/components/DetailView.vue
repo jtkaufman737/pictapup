dfe34c0582ece3481bbe47ec828a7bb4ef33da62
J Kaufman <jkaufman@Js-MacBook-Pro.local>
2018-10-03 20:36:02 -0400

    First commit

---
+++ src/components/DetailView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card-image">
+  </div>
+</template>
+<script>
+import data from '../data'
+
+export default {
+  methods: {
+    displayDetails (id) {
+      this.$router.push({name: 'detail', params: { id: id }})
+    }
+  },
+  data () {
+    return {
+      'pictures': data.pictures
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
---
+++ src/data.js
@@ -0,0 +1,34 @@
+export default {
+  pictures: [
+    {
+      'id': 0,
+      'url': 'https://i.imgur.com/Na2kb1E.png',
+      'comment': '',
+      'info': 'Posted by Kevin on Friday'
+    },
+    {
+      'id': 1,
+      'url': 'https://i.imgur.com/07taUIP.png',
+      'comment': '',
+      'info': 'Posted by Charles on Tuesday'
+    },
+    {
+      'id': 2,
+      'url': 'https://i.imgur.com/Nnuba3J.png',
+      'comment': '',
+      'info': 'Posted by Richard on Monday'
+    },
+    {
+      'id': 3,
+      'url': 'https://i.imgur.com/FSxyKrp.png',
+      'comment': '',
+      'info': 'Posted by Richard on Monday'
+    },
+    {
+      'id': 4,
+      'url': 'https://i.imgur.com/elrRp3O.png',
+      'comment': '',
+      'info': 'Posted by Richard on Monday'
+    }
+  ]
+}
